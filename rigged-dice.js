23a8f97917bfabd162fa61f40028d923045b6c28
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-09-23 11:37:07 -0400

    added js

--- rigged-dice.js
+++ rigged-dice.js
@@ -3,12 +3,13 @@ const prompt = require("prompt-sync")()
 const numOfSides = Number(prompt("Please enter how mny sides you want on the dice: "))
 const riggedNumber = Number(prompt("Please enter the number you want rigged: "))
 
-function pickSides(numOfSides, riggedNumber) {
+function pickSides(numOfSides) {
 
     let dice = numOfSides
+
     return Math.floor(Math.random() * dice) + 1
 
 }
-console.log(pickSides(numOfSides, riggedNumber))
+console.log(pickSides(numOfSides))
 
 // Not Finished!!!
